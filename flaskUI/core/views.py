81e49f4a2ab1e50a702b92a9a8c8f4717bd80a87
Mark <hendriksen-mark@hotmail.com>
2025-08-17 16:15:26 +0200

    Update views.py

--- flaskUI/core/views.py
+++ flaskUI/core/views.py
@@ -11,7 +11,7 @@ import subprocess
 from typing import Any, Union
 
 logger: logging.Logger = logManager.logger.get_logger(__name__)
-serverConfig: dict[str, Any] = configManager.serverConfig.yaml_config
+serverConfig: dict[str, str | int | float | dict] = configManager.serverConfig.yaml_config
 core = Blueprint('core', __name__)
 
 @core.route('/')
@@ -28,128 +28,6 @@ def index() -> str:
     """
     return render_template('index.html')
 
-@core.route('/save')
-def save_config() -> str:
-    """
-    Save the server configuration.
-
-    Args:
-        None
-
-    Returns:
-        str: A message indicating whether the configuration was saved or backed up.
-    """
-    backup = request.args.get('backup', type=str) == "True"
-    configManager.serverConfig.save_config(backup=backup)
-    return "backup config\n" if backup else "config saved\n"
-
-@core.route('/reset_config')
-@flask_login.login_required
-def reset_config() -> str:
-    """
-    Reset the server configuration.
-
-    Args:
-        None
-
-    Returns:
-        str: A message indicating that the configuration was reset.
-    """
-    configManager.serverConfig.reset_config()
-    return "config reset\n"
-
-@core.route('/restore_config')
-@flask_login.login_required
-def restore_config() -> str:
-    """
-    Restore the server configuration from a backup.
-
-    Args:
-        None
-
-    Returns:
-        str: A message indicating that the configuration was restored.
-    """
-    configManager.serverConfig.restore_backup()
-    return "restore config\n"
-
-@core.route('/download_config')
-@flask_login.login_required
-def download_config() -> Response:
-    """
-    Download the server configuration.
-
-    Args:
-        None
-
-    Returns:
-        Response: The server configuration file.
-    """
-    path: str = configManager.serverConfig.download_config()
-    return send_file(path, as_attachment=True)
-
-@core.route('/download_log')
-def download_log() -> Response:
-    """
-    Download the log file.
-
-    Args:
-        None
-
-    Returns:
-        Response: The log file.
-    """
-    path: str = configManager.serverConfig.download_log()
-    return send_file(path, as_attachment=True)
-
-@core.route('/download_debug')
-def download_debug() -> Response:
-    """
-    Download the debug file.
-
-    Args:
-        None
-
-    Returns:
-        Response: The debug file.
-    """
-    path: str = configManager.serverConfig.download_debug()
-    return send_file(path, as_attachment=True)
-
-@core.route('/restart')
-def restart() -> str:
-    """
-    Restart the Python process.
-
-    Args:
-        None
-
-    Returns:
-        str: A message indicating that the process was restarted.
-    """
-    configManager.serverConfig.restart_python()
-    return "restart python with args"
-
-@core.route('/info')
-def info() -> dict[str, str]:
-    """
-    Get system information.
-
-    Args:
-        None
-
-    Returns:
-        dict[str, str]: The system information.
-    """
-    uname: os.uname_result = os.uname()
-    return {
-        "sysname": uname.sysname,
-        "machine": uname.machine,
-        "os_version": uname.version,
-        "os_release": uname.release,
-        "server": subprocess.run("stat -c %y api.py", shell=True, capture_output=True, text=True).stdout.strip(),
-        "webui": subprocess.run("stat -c %y flaskUI/templates/index.html", shell=True, capture_output=True, text=True).stdout.strip()
-    }
 
 @core.route('/login', methods=['GET', 'POST'])
 def login() -> Union[str, Response]:
